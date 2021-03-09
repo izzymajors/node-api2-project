171c18a95eb09c17843eaac3945922f7c791109d
Jonathan-Majors <izzymajors@gmail.com>
2021-03-09 17:14:17 -0600

    Meet MVP

--- api/server.js
+++ api/server.js
@@ -1,2 +1,14 @@
 // implement your server here
+
+const express = require('express');
+const postRouter = require('./posts/posts-router')
+const server = express();
+
 // require your posts router and connect it here
+
+server.use(express.json());
+
+server.use('/api/posts', postRouter); 
+
+
+module.exports = server;
